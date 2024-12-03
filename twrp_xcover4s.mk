49c2653181b54e77a8171c0b2af7d721d1adbab3
skajmer <64442855+skajmer@users.noreply.github.com>
2024-12-03 18:02:37 +0100

    Update and rename omni_xcover4s.mk to twrp_xcover4s.mk

--- twrp_xcover4s.mk
+++ twrp_xcover4s.mk
@@ -10,13 +10,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from xcover4s device
 $(call inherit-product, device/samsung/xcover4s/device.mk)
 
 PRODUCT_DEVICE := xcover4s
-PRODUCT_NAME := omni_xcover4s
+PRODUCT_NAME := twrp_xcover4s
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-G398FN
 PRODUCT_MANUFACTURER := samsung
